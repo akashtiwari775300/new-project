4618573434a1d9004753beda2ffe6541829f21bf
akashtiwari775300 <52470065+akashtiwari775300@users.noreply.github.com>
2019-07-16 11:16:34 -0400

    add new project

---
+++ distance.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int feet,inch;
+	printf("enter the value of feet");
+	scanf("%d",&feet);
+	inch=feet*12;
+	printf("total inches will be:%d\n", inch);
+	
+		return 0;
+}
